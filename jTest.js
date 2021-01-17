5afa2e909a1cd1f19f7d29b813ac41fa5e1ccc67
Trevor Pierce <yeahtrevorpierce@gmail.com>
2021-01-17 17:34:30 -0500

    Starting to clean up some absolute garbage, such as removing unused code, and renaming the index.htm file to index.html

--- jTest.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-    $("#tabler").click(function() {
-        var $tableNew = $("<table><tr><th>ketchup</th><th>price</th><tr><table/>");
-        $("form").append($tableNew);
-        $("form").append("<p>ketchup</p>");
-    });
-});
-
