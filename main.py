3894321dd15daa1a5649751a70f63f2e70620cbb
Trevor Pierce <yeahtrevorpierce@gmail.com>
2021-01-24 15:09:06 -0500

    reworked to start working with a FastAPI backend

---
+++ main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static", html=True), name="static")
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
